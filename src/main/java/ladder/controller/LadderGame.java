e7ba28b7d858cf4d6f901344ce671dcd7a62ee0f
woomin <dal96k@gmail.com>
2020-07-01 19:53:18 +0900

    Modify getPlayersName method name to getUsersName

--- src/main/java/ladder/controller/LadderGame.java
+++ src/main/java/ladder/controller/LadderGame.java
@@ -5,7 +5,7 @@ import ladder.view.Input;
 
 public class LadderGame {
     public static void main(String[] args) {
-        UsersNameReader usersNameReader = Input.getPlayersName();
+        UsersNameReader usersNameReader = Input.getUsersName();
 
     }
 }
--- src/main/java/ladder/view/Input.java
+++ src/main/java/ladder/view/Input.java
@@ -5,7 +5,7 @@ import ladder.domain.UsersNameReader;
 import java.util.Scanner;
 
 public class Input {
-    public static UsersNameReader getPlayersName() {
+    public static UsersNameReader getUsersName() {
         System.out.println("참여할 사람의 이름을 입력하세요. 이름은 쉼표(,)로 구분합니다");
         Scanner scanner = new Scanner(System.in);
 
