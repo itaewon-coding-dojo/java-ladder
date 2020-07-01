fd3780dbaef5d1125d8bf05155da16e0e4cebd30
woomin <dal96k@gmail.com>
2020-07-01 18:35:36 +0900

    Add User class

---
+++ src/test/java/ladder/UserTest.java
@@ -0,0 +1,17 @@
+package ladder;
+
+import ladder.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserTest {
+    @Test
+    @DisplayName("toString")
+    void toStringMethod() {
+        User user = User.from("john");
+
+        assertThat(user.toString()).isEqualTo(" john");
+    }
+}
